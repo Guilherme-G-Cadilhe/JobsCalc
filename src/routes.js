6aad83a4f50ed978e1b3d3228f3dff2532a68f9d
Bobnini <guilhermegcadilhe@gmail.com>
2021-06-23 15:41:43 -0300

    Fix View path render

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
-const express = require("express"); // Biblioteca
+const express = require('express'); // Biblioteca
 
 const routes = express.Router(); // Objeto guarda rotas
 
 const Profile = {
   data: {
-    name: "Guilherme",
-    avatar: "https://github.com/Guilherme-G-Cadilhe.png",
-    "monthly-budget": 3000,
-    "hours-per-day": 5,
-    "days-per-week": 5,
-    "vacation-per-year": 4,
-    "value-hour": 75,
+    name: 'Guilherme',
+    avatar: 'https://github.com/Guilherme-G-Cadilhe.png',
+    'monthly-budget': 3000,
+    'hours-per-day': 5,
+    'days-per-week': 5,
+    'vacation-per-year': 4,
+    'value-hour': 75,
   },
   controllers: {
     index(req, res) {
-      return res.render(views + "profile", { profile: Profile.data });
+      return res.render('profile', { profile: Profile.data });
     },
     update(req, res) {
       //req.body para pegar os dados
@@ -22,20 +22,20 @@ const Profile = {
       // definir quantas semanas tem em um ano : 52
       const weeksPerYear = 52;
       // remover as semanas de ferias das semanas do ano para pegar quantas semanas tem em um mês
-      const weeksPerMonth = (weeksPerYear - data["vacation-per-year"]) / 12;
+      const weeksPerMonth = (weeksPerYear - data['vacation-per-year']) / 12;
       // quantas horas por semana estou trabalhando
-      const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+      const weekTotalHours = data['hours-per-day'] * data['days-per-week'];
       // total de horas trabalhadas no mes
       const monthlyTotalHours = weekTotalHours * weeksPerMonth;
       // Valor da minha hora
-      const valueHour = data["monthly-budget"] / monthlyTotalHours;
+      const valueHour = data['monthly-budget'] / monthlyTotalHours;
 
       Profile.data = {
         ...Profile.data,
         ...req.body,
-        "value-hour": valueHour,
+        'value-hour': valueHour,
       };
-      return res.redirect("/profile");
+      return res.redirect('/profile');
     },
   },
 };
@@ -44,16 +44,16 @@ const Job = {
   data: [
     {
       id: 1,
-      name: "Pizzaria Guloso",
-      "daily-hours": 2,
-      "total-hours": 1,
+      name: 'Pizzaria Guloso',
+      'daily-hours': 2,
+      'total-hours': 1,
       created_at: Date.now(),
     },
     {
       id: 2,
-      name: "OneTwo Project",
-      "daily-hours": 3,
-      "total-hours": 47,
+      name: 'OneTwo Project',
+      'daily-hours': 3,
+      'total-hours': 47,
       created_at: Date.now(),
     },
   ],
@@ -63,21 +63,21 @@ const Job = {
       //Calculos dos Jobs
       const updatedJobs = Job.data.map((job) => {
         const remaining = Job.services.remainingDays(job);
-        const status = remaining <= 0 ? "done" : "progress";
+        const status = remaining <= 0 ? 'done' : 'progress';
 
         return {
           ...job,
           remaining,
           status,
-          budget: Job.services.calculateBudget(job, Profile.data["value-hour"]),
+          budget: Job.services.calculateBudget(job, Profile.data['value-hour']),
         };
       });
 
-      return res.render(views + "index", { jobs: updatedJobs });
+      return res.render('index', { jobs: updatedJobs });
     },
     create(req, res) {
       // Render = Renderiza e compila um template(esj) em html
-      return res.render(views + "job");
+      return res.render('job');
     },
     save(req, res) {
       const lastId = Job.data[Job.data.length - 1]?.id || 0;
@@ -85,11 +85,11 @@ const Job = {
       Job.data.push({
         id: lastId + 1,
         name: req.body.name,
-        "daily-hours": req.body["daily-hours"],
-        "total-hours": req.body["total-hours"],
+        'daily-hours': req.body['daily-hours'],
+        'total-hours': req.body['total-hours'],
         created_at: Date.now(), // atribuindo data criação
       });
-      return res.redirect("/");
+      return res.redirect('/');
     },
     show(req, res) {
       // Pega o id enviado pelo req
@@ -98,16 +98,13 @@ const Job = {
       const job = Job.data.find((job) => Number(job.id) === Number(jobId));
       // Se o id não existir, retornar
       if (!job) {
-        return res.send("Job not found!");
+        return res.send('Job not found!');
       }
       // calcula o preço do job pela função que fica em serviço
       // atualiza o preço do job
-      job.budget = Job.services.calculateBudget(
-        job,
-        Profile.data["value-hour"]
-      );
+      job.budget = Job.services.calculateBudget(job, Profile.data['value-hour']);
       // devolve o job atualizado
-      return res.render(views + "job-edit", { job });
+      return res.render('job-edit', { job });
     },
     update(req, res) {
       // Pega o id enviado pelo req
@@ -116,14 +113,14 @@ const Job = {
       const job = Job.data.find((job) => Number(job.id) === Number(jobId));
       // Se o id não existir, retornar
       if (!job) {
-        return res.send("Job not found!");
+        return res.send('Job not found!');
       }
       // Pega as novas propriedades pelo req para atualizar o job
       const updatedJob = {
         ...job,
         name: req.body.name,
-        "total-hours": req.body["total-hours"],
-        "daily-hours": req.body["daily-hours"],
+        'total-hours': req.body['total-hours'],
+        'daily-hours': req.body['daily-hours'],
       };
       // se o id do req for igual ao que ta no data.id, atualiza o job
       Job.data = Job.data.map((job) => {
@@ -133,7 +130,7 @@ const Job = {
 
         return job;
       });
-      res.redirect("/job/" + jobId);
+      res.redirect('/job/' + jobId);
     },
     delete(req, res) {
       // pega o id pelo req
@@ -142,14 +139,14 @@ const Job = {
       // depois atualiza o array, tendo removido aquele
       Job.data = Job.data.filter((job) => Number(job.id) !== Number(jobId));
 
-      return res.redirect("/");
+      return res.redirect('/');
     },
   },
 
   services: {
     remainingDays(job) {
       // Calculo de tempo restante
-      const remainingDays = (job["total-hours"] / job["daily-hours"]).toFixed();
+      const remainingDays = (job['total-hours'] / job['daily-hours']).toFixed();
 
       const createdDate = new Date(job.created_at);
       // pega número do dia \/ + Calculo escolhido pelo perfil
@@ -166,23 +163,21 @@ const Job = {
       // restam x dias
       return dayDiff;
     },
-    calculateBudget: (job, valueHour) => valueHour * job["total-hours"],
+    calculateBudget: (job, valueHour) => valueHour * job['total-hours'],
   },
 };
 
-// __dirname = variavel que é diretorio atual
-const views = __dirname + "/views/";
-
 // request, response ( Pedido, Resposta)
-routes.get("/", Job.controllers.index);
-routes.get("/job", Job.controllers.create);
-routes.post("/job", Job.controllers.save);
+routes.get('/', Job.controllers.index);
 
-routes.get("/job/:id", Job.controllers.show);
-routes.post("/job/:id", Job.controllers.update);
-routes.post("/job/delete/:id", Job.controllers.delete);
+routes.get('/job', Job.controllers.create);
+routes.post('/job', Job.controllers.save);
 
-routes.get("/profile", Profile.controllers.index);
-routes.post("/profile", Profile.controllers.update);
+routes.get('/job/:id', Job.controllers.show);
+routes.post('/job/:id', Job.controllers.update);
+routes.post('/job/delete/:id', Job.controllers.delete);
+
+routes.get('/profile', Profile.controllers.index);
+routes.post('/profile', Profile.controllers.update);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
-const express = require("express");
+const express = require('express');
 const server = express();
-const routes = require("./routes");
+const routes = require('./routes');
+const path = require('path');
+
 // usando template engine
-server.set("view engine", "ejs");
+server.set('view engine', 'ejs');
+
+// Mudar a localização da pasta Views
+server.set('views', path.join(__dirname, 'views'));
 
 //habilitar arquivos estáticos
-server.use(express.static("public"));
+server.use(express.static('public'));
 
 //Usar o req.body
 server.use(express.urlencoded({ extended: true }));
 
 // Routes
 server.use(routes);
-server.listen(3000, () => console.log("Rodando"));
+server.listen(3000, () => console.log('Rodando'));
