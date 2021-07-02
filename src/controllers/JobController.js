ad56e5210739f8590ffd05e6b89e39bd660f0677
Bobnini <guilhermegcadilhe@gmail.com>
2021-07-02 19:51:54 -0300

    Heroku not working

--- src/controllers/JobController.js
+++ src/controllers/JobController.js
@@ -1,6 +1,7 @@
 const Job = require('../model/Job.js');
 const jobUtils = require('../utils/JobUtils.js');
 const Profile = require('../model/Profile.js');
+
 module.exports = {
   create(req, res) {
     // Render = Renderiza e compila um template(esj) em html
